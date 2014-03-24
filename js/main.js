d5fc9ad2b51303ce1ce327d796879852337f068e
Sonja Farrell <sonjafarrell@gmail.com>
2014-03-24 15:01:14 -0700

    fuck

---
+++ js/main.js
@@ -0,0 +1,49 @@
+// check if item is on screen
+
+
+function slideInView(){
+  
+  var $slide = $('.parallax'),
+      scrollPosition = $(window).scrollTop(),
+      windowHeight = $(window).height();
+      
+  $slide.each(function( index ) {
+    var slideId = $(this).attr('id');
+    
+    if (windowHeight + scrollPosition >= $('#'+ slideId).offset().top) {
+      var parallaxFactor = $('#'+ slideId).offset().top - (scrollPosition + windowHeight);
+      
+      $('.' + slideId +'-bg').css('top', (parallaxFactor*0.1)+'px')
+    }
+  });
+}
+
+
+function playerInView(){
+  
+  var $playButton = $('.parallax'),
+      scrollPosition = $(window).scrollTop(),
+      windowHeight = $(window).height();
+      
+  $slide.each(function( index ) {
+    var slideId = $(this).attr('id');
+    
+    if (windowHeight + scrollPosition >= $('#'+ slideId).offset().top) {
+      var parallaxFactor = $('#'+ slideId).offset().top - (scrollPosition + windowHeight);
+      
+      $('.' + slideId +'-bg').css('top', (parallaxFactor*0.1)+'px')
+    }
+  });
+}
+
+
+
+
+
+$(document).scroll(function(){
+  
+  
+  
+  slideInView();
+
+});
